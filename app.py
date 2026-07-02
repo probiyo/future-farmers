4bd278aea9c9b4da97cc316fd5c0b3ffc3aa93c9
probiyo <31yussuf31@gmail.com>
2026-07-02 17:03:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from datetime import datetime
 import urllib.request
 import json
+import base64
 
 st.title("🌱 Future Farmers - Rize")
 
@@ -10,24 +11,27 @@ hava = st.selectbox("Hava Durumu:", ["Güneşli", "Kapalı", "Yağmurlu", "Sisli
 notlar = st.text_area("Notunuz:")
 camera = st.camera_input("Fotoğraf çek:")
 
-# Sizin oluşturduğunuz Google Apps Script köprü adresi
 URL = "https://script.google.com/macros/s/AKfycbxamU64AHCSNtW3uKjHC0qibj8tYExRKreXZp3iR9TtBc7b0jbs0YFXF_zbleovy0SJ/exec"
 
 if st.button("Veriyi Gönder"):
     if camera:
+        # Fotoğrafı internet üzerinden gönderebilmek için metne dönüştürüyoruz
+        bytes_data = camera.getvalue()
+        base64_photo = base64.b64encode(bytes_data).decode('utf-8')
+        
         veri = {
             "Tarih": datetime.now().strftime("%d-%m-%Y %H:%M"),
             "Gözlem_Turu": gozlem,
             "Hava_Durumu": hava,
             "Notlar": notlar,
-            "Foto_Link": "Fotoğraf eklendi"
+            "Foto_Base64": base64_photo
         }
         
         try:
             data = json.dumps(veri).encode('utf-8')
             req = urllib.request.Request(URL, data=data, headers={'Content-Type': 'application/json'})
             with urllib.request.urlopen(req) as res:
-                st.success("Veri başarıyla arşive gönderildi! Tablonuzu kontrol edebilirsiniz. 🎉")
+                st.success("Veri ve Fotoğraf başarıyla arşive gönderildi! 🎉")
         except Exception as e:
             st.error(f"Bağlantı hatası: {e}")
     else:
